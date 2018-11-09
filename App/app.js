d1912e5b73c6fcedac7510190d2ee4bf3f0bb7a9
met45 <marguerite.tong@gmail.com>
2018-11-09 22:30:17 +0000

    Added functions to app.js

--- App/app.js
+++ App/app.js
@@ -8,8 +8,11 @@ var url = 'mongodb://localhost:27017';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
-
-  db.close();
+	insertDocuments(db, function() {
+    findDocuments(db, function() {
+      db.close();
+    });
+  });
 });
 
 
@@ -29,17 +32,15 @@ var insertDocuments = function(db, callback) {
 
 }
 
- var MongoClient = require('mongodb').MongoClient
-  , assert = require('assert');
 
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-  insertDocuments(db, function() {
-    db.close();
+var findDocuments = function(db, callback) {
+  // Get the documents collection
+  var collection = db.collection('documents');
+  // Find some documents
+  collection.find({}).toArray(function(err, docs) {
+    assert.equal(err, null);
+    console.log("Found the following records");
+    console.log(docs)
+    callback(docs);
   });
-});
+}
