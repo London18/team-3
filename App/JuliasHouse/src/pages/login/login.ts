0e1e8a17181131dc066b19e5575690480a6e530e
Phoebe Yao <phoebe@dahmakan.com>
2018-11-10 03:02:14 +0000

    created login and menu

--- App/JuliasHouse/src/pages/login/login.ts
+++ App/JuliasHouse/src/pages/login/login.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { AppointmentsPage } from '../appointments/appointments';
 
 /**
  * Generated class for the LoginPage page.
@@ -14,8 +15,25 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  @ViewChild('username') username;
+  @ViewChild('password') password;
+  alert: boolean;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.alert = false;
+  }
+
+  public login() {
+    console.log(this.username.value, this.password.value);
+    if(this.username.value == "admin" && this.password.value == "admin") {
+      this.navCtrl.setRoot(AppointmentsPage);
+    } else {
+      this.alert = true;
+    }
+  }
+
+  badCred() {
+      return this.alert;
   }
 
   ionViewDidLoad() {
