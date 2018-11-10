228734ee25a43d36e54358db27f1cc3de704a72f
Phoebe Yao <phoebeyao2@gmail.com>
2018-11-10 03:02:14 +0000

    created login and menu

--- App/JuliasHouse/src/app/app.component.ts
+++ App/JuliasHouse/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
- 
-import { AppointmentsPage } from '../pages/appointments/appointments';
+import { LoginPage } from '../pages/login/login';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = AppointmentsPage;
+  rootPage:any = LoginPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
