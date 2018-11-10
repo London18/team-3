5e0909c5507e070920b348a91816635f538fa650
met45 <marguerite.tong@gmail.com>
2018-11-10 03:57:29 +0000

    Two-way JSON

--- App/fetch.js
+++ App/fetch.js
@@ -1,70 +1,68 @@
 // CLIENT SIDE
 
 // Front end HTML calls functions in fetch.js
-// Fetch.js uses fetch and post to create http call
-// HTTP call is made
+// Fetch.js uses fetch to create http call
+// HTTP call is made to rest API
 
 
 // SERVER SIDE
 
-// HTTP call is picked up by index.js
+// HTTP call is picked up by rest API, index.js
 // NodeJS processes HTTP call
 // Uses Mongoose to talk to the MongoDB
 
 // npm i node-fetch --save
+
 const fetch = require("node-fetch");
-
-/*
-David Walsh fetch documentation
-
-A Request instance represents the request piece of a fetch call. By passing fetch a Request you can make advanced and customized requests:
-
-method - GET, POST, PUT, DELETE, HEAD
-url - URL of the request
-headers - associated Headers object
-referrer - referrer of the request
-mode - cors, no-cors, same-origin
-credentials - should cookies go with the request? omit, same-origin
-redirect - follow, error, manual
-integrity - subresource integrity value
-cache - cache mode (default, reload, no-cache)
-*/
-
-
-
 
 // Get appointments list
 fetch('http://localhost:3000/appointments', {
 	method: 'get'
+
+	// These interpret the response from the rest API
 }).then(function(response) {
-	return response.text();
-}).then(function(text) {
+	return response.json();
+}).then(function(json) {
 	console.log("Here are your appointments:");
-	console.log(text);
+	console.log(json);
 }).catch(function(err) {
 	// Error :()
 });
 
+
+
 // Update status
+data = ["Michael","Ginger","Food"]
 fetch('http://localhost:3000/status', {
-	method: 'put'
+	method: 'put',
+
+	// This sends a request to the rest API containing JSON data
+
+	headers: {"Content-Type": "application/json"},
+
+	body: JSON.stringify(data)
+
+
+
 }).then(function(response) {
-	return response.text();
-}).then(function(text) {
-	console.log("Here are your appointments:");
-	console.log(text);
+	console.log("Status updated");
 }).catch(function(err) {
 	// Error :()
 });
+
+
+
+
 
 
 // Login
 fetch('http://localhost:3000/login', {
 	method: 'get'
+
 }).then(function(response) {
 	return response.text();
 }).then(function(text) {
-	console.log("Here are your appointments:");
+	console.log("Login details matched");
 	console.log(text);
 }).catch(function(err) {
 	// Error :()
@@ -74,12 +72,14 @@ fetch('http://localhost:3000/login', {
 // Create nurse/family/appointment
 fetch('http://localhost:3000/create', {
 	method: 'post'
+
 }).then(function(response) {
 	return response.text();
 }).then(function(text) {
-	console.log("Here are your appointments:");
+	console.log("Object created");
 	console.log(text);
 }).catch(function(err) {
 	// Error :()
 });
+
 
